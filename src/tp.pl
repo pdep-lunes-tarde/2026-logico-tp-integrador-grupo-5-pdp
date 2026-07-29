d9ca23448c6de51ddba17424ae8498adccc80f4f
Esteban <esfernandez@frba.utn.edu.ar>
2026-07-29 16:26:22 -0300

    punto2

--- src/tp.pl
+++ src/tp.pl
@@ -33,9 +33,43 @@ estaVivoEn(Persona, Anio):-
     habitante(Persona, elfo, Nacimiento, _),
     Nacimiento =< Anio.
 
+%Punto2A
+conoce(wirbel, hazania(rescatarHermanaWirbel, [stark, fern], klares), 1390, presencio).
+conoce(frieren, hazania(rescatarHermanaWirbel, [stark, fern], klares), 1390, presencio).
+conoce(lawine, hazania(destruirAura, [frieren], weise), 1393, cancion).
+conoce(voll, hazania(destruirAura, [denken], auberst), 1400, libro(50)).
+conoce(serie, hazania(destruirReyDemonio, [frieren, himmel, heiter, eisen], ende), 1335, libro(100)).
+conoce(kanne, hazania(recuperarGato, [himmel, frieren], eisen), 1375, presencio).
+
+duracionRecuerdo(cancion, 15).
+duracionRecuerdo(libro(Paginas), Paginas).
+
+hazaniaRecordada(Persona, NombreHazania, Anio):-
+    conoce(Persona, hazania(NombreHazania, _, _), AnioConocimiento, presencio),
+    Anio >= AnioConocimiento,
+    estaVivoEn(Persona, Anio).
+
+hazaniaRecordada(Persona, NombreHazania, Anio):-
+    conoce(Persona, hazania(NombreHazania, _, _), AnioConocimiento, MedioDeConocimiento),
+    duracionRecuerdo(MedioDeConocimiento, Duracion),
+    Anio >= AnioConocimiento,
+    Anio =< AnioConocimiento + Duracion.
+
+%Punto2B
+corroborada(Nombre):-
+    conoce(_, hazania(Nombre, Personas, Lugar), _, _),
+    forall(
+        conoce(_, hazania(Nombre, Personas2, Lugar2), _, _),
+        (Personas == Personas2, Lugar == Lugar2)
+    ).
+%Punto2C
+pasoAlOlvido(NombreHazania, Anio):-
+    not(hazaniaRecordada(_, NombreHazania, Anio)).
 
 
-:- begin_tests(tpIntegrador, []).
+
+%Tests
+:- begin_tests(habitantes).
 
 test("humanos y enanos no viven mas alla de su esperanza de vida", nondet):-
     estaVivoEn(kanne, 1370),
@@ -50,4 +84,41 @@ test("nadie puede vivir antes de haber nacido, sin importar su raza"):-
 test("los elfos no mueren de viejos"):-
     estaVivoEn(serie, 5000).
 
-:- end_tests(tpIntegrador).
+:- end_tests(habitantes).
+
+:- begin_tests(recuerdos).
+
+test("Lawine no recuerda destruir al demonio Aura en 1380 porque aún no escuchó una canción sobre esa hazaña", [fail]):-
+    hazaniaRecordada(lawine, destruirAura, 1380).
+
+test("Lawine recuerda destruir al demonio Aura en 1400", nondet):-
+    hazaniaRecordada(lawine, destruirAura, 1400).
+
+test("Lawine ya no recuerda destruir al demonio Aura en 1410 porque pasaron más de 15 años de que escuchó la canción", [fail]):-
+    hazaniaRecordada(lawine, destruirAura, 1410).
+
+test("Voll recuerda destruir al demonio Aura en 1450"):-
+    hazaniaRecordada(voll, destruirAura, 1450).
+
+test("Voll no recuerda destruir al demonio Aura en 1460", [fail]):-
+    hazaniaRecordada(voll, destruirAura, 1460).
+
+test("Wirbel recuerda rescatar a la hermana de Wirbel en 1430", nondet):-
+    hazaniaRecordada(wirbel, rescatarHermanaWirbel, 1430).
+
+test("Wirbel ya no recuerda rescatar a la hermana de Wirbel en 1440 porque no está vivo en ese año", [fail]):-
+    hazaniaRecordada(wirbel, rescatarHermanaWirbel, 1440).
+
+test("Rescatar a la hermana de Wirbel es una hazaña corroborada", nondet):-
+    corroborada(rescatarHermanaWirbel).
+
+test("Destruir al demonio Aura no es una hazaña corroborada", [fail]):-
+    corroborada(destruirAura).
+
+test("Destruir al demonio Aura pasó al olvido en 1460"):-
+    pasoAlOlvido(destruirAura, 1460).
+
+test("Destruir al demonio Aura no pasó al olvido en 1440", [fail]):-
+    pasoAlOlvido(destruirAura, 1440).
+
+:- end_tests(recuerdos).
